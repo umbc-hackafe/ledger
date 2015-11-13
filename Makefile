2e57ad0759945858ccecb89b3efbcdd3eaa8cd79
Alexander Bauer <sasha@linux.com>
2015-11-12 20:26:48 -0500

    Add --output flag

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 sheet.ledger: read_sheet.py purchases.csv payments.csv header.ledger
-	./read_sheet.py > sheet.ledger
+	./read_sheet.py --output sheet.ledger
 
 sample.ledger: read_sheet.py purchases.csv.sample payments.csv.sample header.ledger.sample
 	./read_sheet.py --purchases purchases.csv.sample \
 					--payments  payments.csv.sample \
 					--header    header.ledger.sample \
-					> $@
+					--output    $@
--- read_sheet.py
+++ read_sheet.py
@@ -84,47 +84,51 @@ class Transaction(object):
 
 
 def main(args):
-    # Print the contents of the header file.
-    if args.header:
-        try:
-            with open(args.header, 'r') as f:
-                for line in f:
-                    print(line, end='')
-            print()
+    # Open the outfile if it's a path, or use it directly.
+    out = args.output
+    with open(out, 'w') if type(out) == str else out as of:
+        # Print the contents of the header file.
+        if args.header:
+            try:
+                with open(args.header, 'r') as f:
+                    for line in f:
+                        print(line, end='', file=of)
+                print(file=of)
 
-        except FileNotFoundError:
-            print("Could not open header file, skipping: {}"
-                    .format(args.header),
-                    file = os.stderr)
+            except FileNotFoundError:
+                print("Could not open header file, skipping: {}"
+                        .format(args.header),
+                        file=os.stderr)
 
-    ts = []
+        ts = []
 
-    # Read the purchases file
-    with open(args.purchases, 'r') as f:
-        # Discard the first two lines, which are garbage.
-        next(f)
-        next(f)
+        # Read the purchases file
+        with open(args.purchases, 'r') as f:
+            # Discard the first two lines, which are garbage.
+            next(f)
+            next(f)
 
-        # Read the transactions
-        ts.extend((Transaction(row, "purchase") for row in csv.DictReader(f)))
+            # Read the transactions
+            ts.extend((Transaction(row, "purchase") for row in csv.DictReader(f)))
 
-    # Read the payments file
-    with open(args.payments, 'r') as f:
-        # Read the transactions
-        ts.extend((Transaction(row, "payment") for row in csv.DictReader(f)))
+        # Read the payments file
+        with open(args.payments, 'r') as f:
+            # Read the transactions
+            ts.extend((Transaction(row, "payment") for row in csv.DictReader(f)))
 
-    # Remove invalid transactions.
-    ts = filter(lambda t: t.valid, ts)
+        # Remove invalid transactions.
+        ts = filter(lambda t: t.valid, ts)
 
-    for t in sorted(ts, key=lambda t: t.date):
-        # Print the transaction and its ledger translation
-        print(t.ledger())
-        print()
+        for t in sorted(ts, key=lambda t: t.date):
+            # Print the transaction and its ledger translation
+            print(t.ledger(), file=of)
+            print(file=of)
 
 def parse(args):
     parser = argparse.ArgumentParser()
     parser.add_argument("--purchases", default="purchases.csv")
     parser.add_argument("--payments", default="payments.csv")
+    parser.add_argument("--output", "-o", default=sys.stdout)
     parser.add_argument("--header", default="header.ledger")
 
     return parser.parse_args(args)
