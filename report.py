c22f1106100665bd8ed381bf16a7192027d557b5
Alexander Bauer <sasha@linux.com>
2015-11-14 19:06:50 -0500

    Add text wrapping to reports

--- report.py
+++ report.py
@@ -4,6 +4,8 @@ import datetime
 import subprocess
 import argparse
 
+import textwrap
+
 def main(args):
     ledger_args = {
             'ledger': args.ledger,
@@ -21,13 +23,42 @@ def main(args):
             args.debt_cmd.format(**ledger_args).split()
         ).decode('utf-8')
 
+    report = \
+"""{intro_text}
+{column_information}
+ - Actual purchases
+ - Maximum monthly budget
+ - Percent of budget expended
+ - Category
+
+{budget_report}
+
+{owe_text}
+
+{owe_addendum}
+
+{owe_report}
+""".format(
+    intro_text = textwrap.fill("""This is the monthly Hackafé finance report. The
+following table shows our expenses plotted against our budget, grouped by
+category.""", width = args.width),
+    column_information = textwrap.fill("""The columns from left to right
+        are:""", width = args.width),
+    budget_report = budget_output,
+    owe_text = textwrap.fill("""The amount that you owe is shown below. It is the
+sum of Hackafé's liabilities to you (amounts you have paid on our behalf that
+have not been repaid by others), assets (amounts you owe as result of a purchase
+on your behalf) and income (amounts you have paid to others).""",
+        width = args.width),
+    owe_addendum = textwrap.fill("""If the amount is negative, Hackafé owes you
+money.""", width = args.width),
+    owe_report = "{owe_report}")
+
     if args.action == 'print':
-        print(budget_output)
         for person in args.people:
-            print()
-            print('-'*len(person))
-            print(person)
-            print(debt_output[person])
+            print('- {} {}'.format(person,
+                '-' * (args.width - len(person) - 3)))
+            print(report.format(owe_report = debt_output[person]))
 
 def parse(args):
     parser = argparse.ArgumentParser()
@@ -40,6 +71,7 @@ def parse(args):
         default='{ledger} -f {file} balance {person}')
     parser.add_argument('--month',
         default=datetime.datetime.now().strftime('%Y/%m'))
+    parser.add_argument('--width', type=int, default=78)
     parser.add_argument('--file', default='sheet.ledger')
     return parser.parse_args(args)
 
